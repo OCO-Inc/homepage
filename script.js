6bc3f78758fbb29838d33825ed8428b970fd7c08
OCO-Inc <69772257+OCO-Inc@users.noreply.github.com>
2024-08-10 01:16:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ window.onload = function() {
       var currentTime = Date.now();
 	  ctx.fillStyle = "white"
       ctx.font = "6px caption";
-      ctx.fillText("Move your mouse around to collect stars, and click to release them!", 0, 0);
+      ctx.fillText("Move your mouse around to collect stars, and click to release them!", 8, 8);
 	  
       for (var i = P.length - 1; i >= 0; i--) {
          p = P[i];
