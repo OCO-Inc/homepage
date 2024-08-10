f458f64d7d9baccf342e5ac2aade5435cd7f875a
OCO-Inc <69772257+OCO-Inc@users.noreply.github.com>
2024-08-10 01:02:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ window.onload = function() {
 		   vx = 0
 		   vy = 0
 		 }
-         r = rand(1, 3);
+         r = rand(0.01, 3);
 		 if (r > 2.8) {
-		   r = rand(3, 10);
+		   r = rand(1, 8);
 		 }
 		 
          red = Math.round(rand(235, 255));
