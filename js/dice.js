ffc5f5b83f48a50d8f03ecd8d46655b868588e24
qwikster <ocoincs@gmail.com>
2025-01-26 18:54:21 -0500

    plswork

--- js/dice.js
+++ js/dice.js
@@ -21,7 +21,50 @@ function roll() { // actually roll the dice. called when the "roll" button is cl
   for (let i = 0; i < (document.getElementById("times").value * document.getElementById("count").value); i++) { // repeat the folowing (number of dice * times to roll) times
     rollList.push(random(1, document.getElementById("sides").value)) // roll once for each time it is told and add it to the list of rolls
   }
-  document.getElementById("rolls").innerText = rollList
+  render();
+}
+
+function countOccurrences(arr) { //Function to generate the data we need for the graph
+  const countMap = {};
+
+  // Count occurrences of each number
+  arr.forEach(num => {
+    countMap[num] = (countMap[num] || 0) + 1;
+  });
+
+  // Determine the range of numbers (from min to max)
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  // Create an array of counts for each number from min to max
+  const result = [];
+  for (let i = min; i <= max; i++) {
+    result.push(countMap[i] || 0); // If the number isn't present, add 0
+  }
+
+  return result;
+}
+
+function render() { //generate information and show our graph
+  var xValues = Array.from({ length: document.getElementById("sides").value }, (_, i) => (i + 1).toString()); // some really weird logic that generates the X axis labels for our chart
+  let yValues = countOccurrences(rollList); //use the function we made earlier to generate our data
+  const chart = new Chart("graph", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: aqua,
+        data: yValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      title: {
+        display: true,
+        text: "Graph of Resulting Rolls"
+      }
+    }
+  });
 }
 
 loop(); //start here
