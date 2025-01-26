30f523a96cb21ef86f5171b88c3ead1539d319fb
qwikster <ocoincs@gmail.com>
2025-01-26 17:50:09 -0500

    a

--- js/dice.js
+++ js/dice.js
@@ -1,15 +1,4 @@
-function width() {
-  if (window.innerWidth < window.innerHeight) {
-    document.getElementById("slidecontainer").style.width = window.innerWidth;
-    document.getElementById("slidecontainer").style.height = window.innerHeight/8;
-  } else {
-    document.getElementById("slidecontainer").style.width = window.innerWidth;
-    document.getElementById("slidecontainer").style.height = window.innerHeight/8;
-  }
-}
-
 function loop() {
-  width();
   display();
   window.requestAnimationFrame(loop);
 }
@@ -24,10 +13,8 @@ var count = document.getElementById("count");
 var countout = document.getElementById("countout");
 var sidesout = document.getElementById("sidesout");
 
-width();
-loop();
-
 function render() {
   document.getElementById("test").style.visibility = "visible";
 }
 
+loop();
