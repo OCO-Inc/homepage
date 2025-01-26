24085bbddfae6b08fe26e34de46d7f97cd3b245e
qwikster <ocoincs@gmail.com>
2025-01-26 17:59:33 -0500

    dice

--- js/dice.js
+++ js/dice.js
@@ -6,10 +6,7 @@ function loop() {
 function display() {
   document.getElementById('sidesout').innerText = document.getElementById("sides").value;
   document.getElementById('countout').innerText = document.getElementById("count").value;
-}
-
-function render() {
-  document.getElementById("test").style.visibility = "visible";
+  document.getElementById('timesout').innerText = document.getElementById("times").value;
 }
 
 loop();
