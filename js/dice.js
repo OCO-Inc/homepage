e4662d6952227d02013a21395c2da6ad097a58f1
qwikster <ocoincs@gmail.com>
2025-01-26 18:22:43 -0500

    idiocy repair

--- js/dice.js
+++ js/dice.js
@@ -17,11 +17,11 @@ function random (min, max) { // simple random function, returns between min and 
 }
 
 function roll() { // actually roll the dice. called when the "roll" button is clicked
-  roll.length = 0 // ensure the list of dice rolls already completed is 0
+  rollList.length = 0 // ensure the list of dice rolls already completed is 0
   for (let i = 0; i < (document.getElementById("times").value * document.getElementById("count").value); i++) { // repeat the folowing (number of dice * times to roll) times
-    roll.push(random(1, document.getElementById("sides"))) // roll once for each time it is told and add it to the list of rolls
+    rollList.push(random(1, document.getElementById("sides"))) // roll once for each time it is told and add it to the list of rolls
   }
-  document.getElementById("rolls").innerText = roll
+  document.getElementById("rolls").innerText = rollList
 }
 
 loop(); //start here
